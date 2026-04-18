80feb88fa1ed685c64caa9dbd4b1f821f30ddfe0
egus05 <egus05@DESKTOP-VSP71BC>
2026-04-19 00:31:50 +0900

    coverage 구현 및 오류수정

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ import timm
 os.makedirs('results',exist_ok=True)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 base_path = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(base_path, 'models', 'my_resnet50_cifar10.pth')
-
+modelA_path = os.path.join(base_path, 'models', 'resnet50_cifar10_A.pth')
+modelB_path = os.path.join(base_path, 'models', 'resnet50_cifar10_B.pth')
 
 cifar10_transform_test = transforms.Compose([
     transforms.ToTensor(),
@@ -22,33 +22,64 @@ cifar10_transform_test = transforms.Compose([
 cifar10_test = torchvision.datasets.CIFAR10(root='./data',train=False,download=True,transform=cifar10_transform_test)
 cifar10_testLoader = torch.utils.data.DataLoader(cifar10_test,batch_size=1,shuffle=True)
 
+def load_model(path):
+    model = models.resnet50(weights=None)
+    model.fc = torch.nn.Sequential(
+        torch.nn.Linear(model.fc.in_features,256),
+        torch.nn.ReLU(),
+        torch.nn.Linear(256,10)
+    )
+    model.load_state_dict(torch.load(path,map_location=device))
+    model.to(device)
+    model.eval()
+    return model
+
 #모델 load
 try:
-    resnet50_cifar10 = timm.create_model("resnet50_cifar10", pretrained=True).to(device)
-    resnet50_cifar10.eval()
+    """
+    #resnet50_cifar10 = timm.create_model("resnet50_cifar10", pretrained=True).to(device)
+    #resnet50_cifar10.eval()
     
     my_resnet50_cifar10 = models.resnet50(weights=None)
     my_resnet50_cifar10.fc = torch.nn.Linear(my_resnet50_cifar10.fc.in_features,10) #class 10개로 수정
-    my_resnet50_cifar10.load_state_dict(torch.load(model_path))
+    my_resnet50_cifar10.load_state_dict(torch.load(modelA_path))
     my_resnet50_cifar10.to(device)
     my_resnet50_cifar10.eval()
+    """
     
-    Models = [resnet50_cifar10,my_resnet50_cifar10]
+    modelA = load_model(modelA_path)
+    modelB = load_model(modelB_path)
+    
+    Models = [modelA,modelB]
     print('load success')
     
-except:
-    print("load failed")
+except Exception as e:
+    print(f"load failed {e}")
+    exit()
     
-activated_neurons = {}
+activated_neurons = set() #활성화 된 뉴런을 저장
+current_layer_output = None
+
+#active된 뉴런을 탐지
+def coverage_hook(module,input,output):
+    global current_layer_output
+    current_layer_output = output
+    
+    active = (output > 0).nonzero(as_tuple=False) #coverage 측정용
+    for idx in active:
+        activated_neurons.add(tuple(idx[1:].tolist()))
 
 def deepxplore_loss(output,model_idx):
     loss_diff = 0
+    lambda_val = 0.1
     cur_output = output[model_idx]
     for i,out in enumerate(output):
         if i != model_idx:
             loss_diff += torch.norm(cur_output - out,p=2)
+            
+    loss_coverage = torch.mean(current_layer_output)
     
-    return -loss_diff
+    return -(loss_diff + lambda_val * loss_coverage)
 
 def gen_diff_pytorch(Models,input,iteration=50,lr=0.01):
     input = input.clone().detach().to(device).requires_grad_(True)
@@ -77,6 +108,7 @@ def gen_diff_pytorch(Models,input,iteration=50,lr=0.01):
 def unnormalize(t):
     return t*0.5 + 0.5
 
+Models[0].layer4[-1].register_forward_hook(coverage_hook)
 disagreed_cnt = 0
 for i,(img,label) in enumerate(cifar10_testLoader):
     if disagreed_cnt >= 5:
@@ -97,15 +129,17 @@ for i,(img,label) in enumerate(cifar10_testLoader):
         
         fig = plt.figure()
         fig.tight_layout()
-                
+
         #원본 이미지
         ax1 = fig.add_subplot(131)
         ax1.set_title('Original')
+        ax1.set_xlabel(f'predict : {pred[0]}')
         ax1.imshow(ori)
         
         #Adversial 이미지
         ax2 = fig.add_subplot(132)
         ax2.set_title('Adversarial')
+        ax2.set_xlabel(f'predict : {pred[1]}')
         ax2.imshow(adv)
         
         #차이를 확대한 이미지
@@ -116,3 +150,8 @@ for i,(img,label) in enumerate(cifar10_testLoader):
         save = f'results/dissagreement_{i}.png'
         plt.savefig(save)
         plt.close()
+    
+    
+total_nuerons = 2048*4*4
+coverage = len(activated_neurons) / total_nuerons
+print(f"Current Neuron coverage {coverage:.2f}")
